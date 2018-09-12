34d608d1565e07a1aef74839c207d583617bb1de
penglei <2643650181@qq.com>
2018-09-12 18:49:54 +0800

    封装 Eigen 库

---
+++ imgprocess/src/core/mattools/mattool_common.h
@@ -0,0 +1,46 @@
+//
+// Created by penglei on 18-9-12.
+//
+
+#ifndef IMGPROCESS_MATTOOL_COMMON_H
+#define IMGPROCESS_MATTOOL_COMMON_H
+
+#include "Eigen/Dense"
+using namespace Eigen;
+
+namespace MatTool {
+
+    // 数据存储在矩阵内部
+    using Matrixub = Eigen::Matrix<unsigned char, Dynamic, Dynamic, RowMajor>;
+    using Matrixus = Eigen::Matrix<unsigned short, Dynamic, Dynamic, RowMajor>;
+    using Matrixs = Eigen::Matrix<short, Dynamic, Dynamic, RowMajor>;
+    using Matrixui = Eigen::Matrix<unsigned int, Dynamic, Dynamic, RowMajor>;
+    using Matrixi = Eigen::Matrix<int, Dynamic, Dynamic, RowMajor>;
+    using Matrixf = Eigen::Matrix<float, Dynamic, Dynamic, RowMajor>;
+    using Matrixd = Eigen::Matrix<double, Dynamic, Dynamic, RowMajor>;
+
+    using Vectord = Eigen::Matrix<double, Dynamic, 1>;  // 列向量
+    using RVectord = Eigen::Matrix<double, 1, Dynamic>;  // 行向量
+
+    // 数据存储在外部（比如存储在外部数组中）
+    using ExtMatrixub = Eigen::Map<Matrixub>;
+    using ExtMatrixus = Eigen::Map<Matrixus>;
+    using ExtMatrixs = Eigen::Map<Matrixs>;
+    using ExtMatrixui = Eigen::Map<Matrixui>;
+    using ExtMatrixi = Eigen::Map<Matrixi>;
+    using ExtMatrixf = Eigen::Map<Matrixf>;
+    using ExtMatrixd = Eigen::Map<Matrixd>;
+
+    using ExtVectord = Eigen::Map<Vectord>;
+    using ExtRVectord = Eigen::Map<RVectord>;
+
+    // 判断矩阵是否可逆
+    template <typename _MatrixType>
+    inline bool isInvertible(const _MatrixType &mat) {
+        Eigen::FullPivLU<_MatrixType> lu(mat);
+        return (bool)lu.isInvertible();
+    }
+
+}
+
+#endif //IMGPROCESS_MATTOOL_COMMON_H
